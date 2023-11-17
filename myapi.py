18ad30fff90f49023a1cc9b261cbfe1d53182b07
raffaele.loglisci <dev03@ormalab.it>
2023-11-17 10:46:20 +0100

    added function to show up simple pdf blank file

--- myapi.py
+++ myapi.py
@@ -1,9 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import StreamingResponse
+from reportlab.pdfgen import canvas
+from io import BytesIO
 
 
 app = FastAPI()
 
 
-@app.get("/ping")
-def pong():
-    return {"ping": "pong!"}
+@app.get("/download_blank_pdf")
+def download_blank_pdf():
+    # Generate a blank PDF
+    pdf_bytes = generate_blank_pdf()
+
+    # Return the PDF as a response
+    return StreamingResponse(BytesIO(pdf_bytes), media_type="application/pdf", headers={"Content-Disposition": "attachment;filename=blank.pdf"})
+
+
+def generate_blank_pdf():
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer)
+
+    # Set up your PDF content here
+    p.drawString(100, 100, "This is a blank PDF.")
+
+    p.showPage()
+    p.save()
+
+    buffer.seek(0)
+    return buffer.read()
