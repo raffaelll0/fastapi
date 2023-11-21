62334f155f179898c6b6669dbb70d8cdf34fbde8
raffaele.loglisci <dev03@ormalab.it>
2023-11-21 10:25:54 +0100

    change of function to call

--- myapi.py
+++ myapi.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 from io import BytesIO
-from monday_data_extraction import main
+from monday_data_extraction import pdf_gen
 
 
 app = FastAPI()
@@ -10,7 +10,7 @@ app = FastAPI()
 @app.get("/")
 def download_blank_pdf():
     #call the genera pdf function in the main file
-    pdf_bytes = main.genera_pdf_prova()
+    pdf_bytes = pdf_gen.create_pdf(file_path="pdf_with_image.pdf")
     # Return the PDF as a response
     return StreamingResponse(BytesIO(pdf_bytes), media_type="application/pdf", headers={"Content-Disposition": "attachment;filename=blank.pdf"})
 
