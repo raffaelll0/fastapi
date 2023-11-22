64cafde565a4bcab7f04e9d875e014c460fb3840
raffaele.loglisci <dev03@ormalab.it>
2023-11-22 11:13:53 +0100

    uncomment to get the full pdf

--- monday_data_extraction/pdf_gen.py
+++ monday_data_extraction/pdf_gen.py
@@ -127,203 +127,203 @@ def create_pdf(file_path):
     # HEADER START
     header(pdf)
     # HEADER END
-#
-#     # TITLE START
-#     title(pdf, text="REPORT KPI MESE 2023", y_offset=680)
-#     # TITLE END
-#
-#     # DESCRIPTIONS START
-#     descriptions(pdf, text_1="Autore del report: Christian Trocino", text2="Dati aggiornati al XX/XX/XXXX", x_offset=50,
-#                  y_offset=650)
-#     # DESCRIPTIONS END
-#
-#     # TITLE_2 START
-#     title(pdf, text="PERIODO DI RIFERIMENTO: MESE 2023", y_offset=600)
-#     # TITLE_2 END
-#
-#     # BOX_1 START
-#     #TAKE ALL THE VALUES FROM DATA TO SCORE
-#     tot_prev_evasi_mese = data_to_score.n_tot_prev_evasi_mese()
-#     tot_prev_acc_mese = data_to_score.n_tot_prev_accettati_mese()
-#     tot_prev_acc_cons = data_to_score.prev_acc_consuntivo()
-#     tot_prev_evasi_tot = data_to_score.importo_tot_prev_evasi()
-#
-#     num_boxes = 4
-#     box_titles = ["N. Tot. Prev. Evasi", "N. Tot. Prev. Accettati", "N. Tot. Prev. Acc. consuntivo ",
-#                   "Importo. Tot. Prev. Evasi"]
-#     box_values = [str(tot_prev_evasi_mese), str(tot_prev_acc_mese), str(tot_prev_acc_cons), str(tot_prev_evasi_tot)]
-#     box_width = 120
-#     box_height = 80
-#
-#     draw_boxes(pdf, num_boxes, box_titles, box_values, box_width, box_height, current_y_offset=480)
-#     # BOX_1 END
-#
-#     #TITLE_3 START
-#     title(pdf, text="PERIODO DI RIFERIMENTO: ANNO 2023", y_offset=450)
-#     # TITLE_3 END
-#
-#     # BOX_2 START
-#     # TAKE ALL THE VALUES FROM DATA TO SCORE
-#     tot_prev_acc = data_to_score.n_tot_prev_accettati_anno()
-#     tot_prev_acc_importo = data_to_score.importo_tot_prev_accettati()
-#
-#
-#     num_boxes = 2
-#     box_titles = ["N. Tot. Prev. Accettati", "Importo. Tot. Prev. Accettati"]
-#     box_values = [str(tot_prev_acc), str(tot_prev_acc_importo)]
-#     box_width = 120
-#     box_height = 80
-#
-#     draw_boxes(pdf, num_boxes, box_titles, box_values, box_width, box_height, current_y_offset=340)
-#     # BOX_2 END
-#
-#     # BOX_3 START
-#     # TAKE ALL THE VALUES FROM DATA TO SCORE
-#     fatturato_ad_oggi = data_to_score.fatturato_ad_oggi()
-#     fatturato_da_emettere = data_to_score.fatturato_da_emettere()
-#     fatturato_previsto = data_to_score.fatturato_prev_2023()
-#
-#     num_boxes = 3
-#     box_titles = ["FATTURATO AD OGGI", "FATTURATO DA EMETTERE", "FATTURATO PREVISTO 2023"]
-#     box_values = [str(fatturato_ad_oggi), str(fatturato_da_emettere), str(fatturato_previsto)]
-#     box_width = 120
-#     box_height = 80
-#
-#     draw_boxes(pdf, num_boxes, box_titles, box_values, box_width, box_height, current_y_offset=240)
-#     # BOX_3 END
-#
-#
-#
-#
-#     # FOOTER START
-#     footer(pdf)
-#     # FOOTER END
-# ########################################################################################################################
-#     # SECOND PAGE START
-#     pdf.showPage()
-#     # HEADER START
-#     header(pdf)
-#     # HEADER END
-#
-#     # TITLE_3 START
-#     title(pdf, text="ANALISI OPERATIVA PROGETTI", y_offset=680)
-#     # TITLE_3 END
-#
-#     # DESCRIPTIONS + IMG START
-#
-#     descriptions(pdf, text_1="N Progetti in progress su PM", text2="", x_offset=50, y_offset=660)
-#     # Draw Altair chart image under the description
-#     chart_path = data_to_chart.n_progetti_in_progress_su_pm()
-#     #chart_path = r'pngs_of_charts/chart_1.png'
-#     draw_chart_under_description(pdf, chart_path, y_offset=670, logo_width=450, logo_height=200)
-#
-#     descriptions(pdf, text_1="Importi Progetti in progress per ANNO", text2="", x_offset=50, y_offset=450)
-#     chart_path = data_to_chart.importo_progetti_progress_anno()
-#     #chart_path = r'C:\Users\raffaele.loglisci\PycharmProjects\pdf_heroku\monday_data_extraction\pngs_of_charts\chart_2.png'
-#     draw_chart_under_description(pdf, chart_path, y_offset=465, logo_width=450, logo_height=180)
-#
-#     descriptions(pdf, text_1="Importo Progetti in progress per BU", text2="", x_offset=50, y_offset=250)
-#     chart_path = data_to_chart.portafoglio_ordine_residuo()
-#     #chart_path = r'C:\Users\raffaele.loglisci\PycharmProjects\pdf_heroku\monday_data_extraction\pngs_of_charts\chart_3.png'
-#     draw_chart_under_description(pdf, chart_path, y_offset=265, logo_width=450, logo_height=130)
-#
-#     # DESCRIPTIONS + IMG END
-#
-#     # FOOTER START
-#     footer(pdf)
-#     # FOOTER END
-#
-#     # SECOND PAGE END
-# ########################################################################################################################
-#     # THIRD PAGE START
-#     pdf.showPage()
-#     # HEADER START
-#     header(pdf)
-#     # HEADER END
-#
-#     # TITLE_4 START
-#     title(pdf, text="CONTROLLO DI GESTIONE", y_offset=680)
-#     # TITLE_4 END
-#
-#     # DESCRIPTIONS + IMG START
-#
-#     descriptions(pdf, text_1="Analisi Ferie - Malattia - Dayhospital - Dipendenti", text2="", x_offset=50, y_offset=660)
-#     # Draw Altair chart image under the description
-#     chart_path = data_to_chart.analisi_ferie_malattia()
-#     #chart_path = r'pngs_of_charts/chart_4.png'
-#     draw_chart_under_description(pdf, chart_path, y_offset=670, logo_width=450, logo_height=200)
-#
-#     descriptions(pdf, text_1="Analisi Permessi/ROL Dipendenti", text2="", x_offset=50, y_offset=450)
-#     chart_path = data_to_chart.analisi_permessi_rol()
-#     #chart_path = r'pngs_of_charts\chart_5.png'
-#     draw_chart_under_description(pdf, chart_path, y_offset=465, logo_width=450, logo_height=180)
-#
-#     descriptions(pdf, text_1="Analisi giornate Smart Working", text2="", x_offset=50, y_offset=250)
-#     chart_path = data_to_chart.giornate_smart_working()
-#     #chart_path = r'pngs_of_charts\chart_6.png'
-#     draw_chart_under_description(pdf, chart_path, y_offset=265, logo_width=450, logo_height=130)
-#
-#     # DESCRIPTIONS + IMG END
-#
-#
-#     # FOOTER START
-#     footer(pdf)
-#     # FOOTER END
-#     # THIRD PAGE END
-#
-# ########################################################################################################################
-#
-#     # FOURTH PAGE START
-#     pdf.showPage()
-#     # HEADER START
-#     header(pdf)
-#     # HEADER END
-#
-#     # TITLE_4 START
-#     title(pdf, text="CONTROLLO DI GESTIONE", y_offset=680)
-#     # TITLE_4 END
-#
-#     # DESCRIPTIONS + IMG START
-#
-#     descriptions(pdf, text_1="Analisi Assenze Liberi Professionisti", text2="", x_offset=50, y_offset=660)
-#     # Draw Altair chart image under the description
-#     chart_path = data_to_chart.analisi_assenze_liberi_professionisti()
-#     #chart_path = r'pngs_of_charts/chart_7.png'
-#     draw_chart_under_description(pdf, chart_path, y_offset=670, logo_width=450, logo_height=200)
-#
-#     descriptions(pdf, text_1="Timesheet Mese", text2="", x_offset=50, y_offset=450)
-#     chart_path = data_to_chart.timesheet_mese()
-#     #chart_path = r'pngs_of_charts\chart_8.png'
-#     draw_chart_under_description(pdf, chart_path, y_offset=465, logo_width=450, logo_height=180)
-#
-#     descriptions(pdf, text_1="BU/h", text2="", x_offset=50, y_offset=250)
-#     chart_path = data_to_chart.bu_h_pie()
-#     #chart_path = r'pngs_of_charts\chart_9.png'
-#     draw_chart_under_description(pdf, chart_path, y_offset=265, logo_width=450, logo_height=130)
-#
-#     # DESCRIPTIONS + IMG END
-#
-#     # FOOTER START
-#     footer(pdf)
-#     # FOOTER END
-#     # FOURTH PAGE END
-# ########################################################################################################################
-#     # FIFTH PAGE START
-#     pdf.showPage()
-#     # HEADER START
-#     header(pdf)
-#     # HEADER END
-#
-#     # BOX_1 START
-#
-#     num_boxes = 2
-#     box_titles = ["OBIETTIVI 2023", "COMMENTI"]
-#     box_values = ["TESTO", "TESTO"]
-#     box_width = 220
-#     box_height = 80
-#
-#     draw_boxes(pdf, num_boxes, box_titles, box_values, box_width, box_height, current_y_offset=600)
-#     # BOX_1 END
+
+    # TITLE START
+    title(pdf, text="REPORT KPI MESE 2023", y_offset=680)
+    # TITLE END
+
+    # DESCRIPTIONS START
+    descriptions(pdf, text_1="Autore del report: Christian Trocino", text2="Dati aggiornati al XX/XX/XXXX", x_offset=50,
+                 y_offset=650)
+    # DESCRIPTIONS END
+
+    # TITLE_2 START
+    title(pdf, text="PERIODO DI RIFERIMENTO: MESE 2023", y_offset=600)
+    # TITLE_2 END
+
+    # BOX_1 START
+    #TAKE ALL THE VALUES FROM DATA TO SCORE
+    tot_prev_evasi_mese = data_to_score.n_tot_prev_evasi_mese()
+    tot_prev_acc_mese = data_to_score.n_tot_prev_accettati_mese()
+    tot_prev_acc_cons = data_to_score.prev_acc_consuntivo()
+    tot_prev_evasi_tot = data_to_score.importo_tot_prev_evasi()
+
+    num_boxes = 4
+    box_titles = ["N. Tot. Prev. Evasi", "N. Tot. Prev. Accettati", "N. Tot. Prev. Acc. consuntivo ",
+                  "Importo. Tot. Prev. Evasi"]
+    box_values = [str(tot_prev_evasi_mese), str(tot_prev_acc_mese), str(tot_prev_acc_cons), str(tot_prev_evasi_tot)]
+    box_width = 120
+    box_height = 80
+
+    draw_boxes(pdf, num_boxes, box_titles, box_values, box_width, box_height, current_y_offset=480)
+    # BOX_1 END
+
+    #TITLE_3 START
+    title(pdf, text="PERIODO DI RIFERIMENTO: ANNO 2023", y_offset=450)
+    # TITLE_3 END
+
+    # BOX_2 START
+    # TAKE ALL THE VALUES FROM DATA TO SCORE
+    tot_prev_acc = data_to_score.n_tot_prev_accettati_anno()
+    tot_prev_acc_importo = data_to_score.importo_tot_prev_accettati()
+
+
+    num_boxes = 2
+    box_titles = ["N. Tot. Prev. Accettati", "Importo. Tot. Prev. Accettati"]
+    box_values = [str(tot_prev_acc), str(tot_prev_acc_importo)]
+    box_width = 120
+    box_height = 80
+
+    draw_boxes(pdf, num_boxes, box_titles, box_values, box_width, box_height, current_y_offset=340)
+    # BOX_2 END
+
+    # BOX_3 START
+    # TAKE ALL THE VALUES FROM DATA TO SCORE
+    fatturato_ad_oggi = data_to_score.fatturato_ad_oggi()
+    fatturato_da_emettere = data_to_score.fatturato_da_emettere()
+    fatturato_previsto = data_to_score.fatturato_prev_2023()
+
+    num_boxes = 3
+    box_titles = ["FATTURATO AD OGGI", "FATTURATO DA EMETTERE", "FATTURATO PREVISTO 2023"]
+    box_values = [str(fatturato_ad_oggi), str(fatturato_da_emettere), str(fatturato_previsto)]
+    box_width = 120
+    box_height = 80
+
+    draw_boxes(pdf, num_boxes, box_titles, box_values, box_width, box_height, current_y_offset=240)
+    # BOX_3 END
+
+
+
+
+    # FOOTER START
+    footer(pdf)
+    # FOOTER END
+########################################################################################################################
+    # SECOND PAGE START
+    pdf.showPage()
+    # HEADER START
+    header(pdf)
+    # HEADER END
+
+    # TITLE_3 START
+    title(pdf, text="ANALISI OPERATIVA PROGETTI", y_offset=680)
+    # TITLE_3 END
+
+    # DESCRIPTIONS + IMG START
+
+    descriptions(pdf, text_1="N Progetti in progress su PM", text2="", x_offset=50, y_offset=660)
+    # Draw Altair chart image under the description
+    chart_path = data_to_chart.n_progetti_in_progress_su_pm()
+    #chart_path = r'pngs_of_charts/chart_1.png'
+    draw_chart_under_description(pdf, chart_path, y_offset=670, logo_width=450, logo_height=200)
+
+    descriptions(pdf, text_1="Importi Progetti in progress per ANNO", text2="", x_offset=50, y_offset=450)
+    chart_path = data_to_chart.importo_progetti_progress_anno()
+    #chart_path = r'C:\Users\raffaele.loglisci\PycharmProjects\pdf_heroku\monday_data_extraction\pngs_of_charts\chart_2.png'
+    draw_chart_under_description(pdf, chart_path, y_offset=465, logo_width=450, logo_height=180)
+
+    descriptions(pdf, text_1="Importo Progetti in progress per BU", text2="", x_offset=50, y_offset=250)
+    chart_path = data_to_chart.portafoglio_ordine_residuo()
+    #chart_path = r'C:\Users\raffaele.loglisci\PycharmProjects\pdf_heroku\monday_data_extraction\pngs_of_charts\chart_3.png'
+    draw_chart_under_description(pdf, chart_path, y_offset=265, logo_width=450, logo_height=130)
+
+    # DESCRIPTIONS + IMG END
+
+    # FOOTER START
+    footer(pdf)
+    # FOOTER END
+
+    # SECOND PAGE END
+########################################################################################################################
+    # THIRD PAGE START
+    pdf.showPage()
+    # HEADER START
+    header(pdf)
+    # HEADER END
+
+    # TITLE_4 START
+    title(pdf, text="CONTROLLO DI GESTIONE", y_offset=680)
+    # TITLE_4 END
+
+    # DESCRIPTIONS + IMG START
+
+    descriptions(pdf, text_1="Analisi Ferie - Malattia - Dayhospital - Dipendenti", text2="", x_offset=50, y_offset=660)
+    # Draw Altair chart image under the description
+    chart_path = data_to_chart.analisi_ferie_malattia()
+    #chart_path = r'pngs_of_charts/chart_4.png'
+    draw_chart_under_description(pdf, chart_path, y_offset=670, logo_width=450, logo_height=200)
+
+    descriptions(pdf, text_1="Analisi Permessi/ROL Dipendenti", text2="", x_offset=50, y_offset=450)
+    chart_path = data_to_chart.analisi_permessi_rol()
+    #chart_path = r'pngs_of_charts\chart_5.png'
+    draw_chart_under_description(pdf, chart_path, y_offset=465, logo_width=450, logo_height=180)
+
+    descriptions(pdf, text_1="Analisi giornate Smart Working", text2="", x_offset=50, y_offset=250)
+    chart_path = data_to_chart.giornate_smart_working()
+    #chart_path = r'pngs_of_charts\chart_6.png'
+    draw_chart_under_description(pdf, chart_path, y_offset=265, logo_width=450, logo_height=130)
+
+    # DESCRIPTIONS + IMG END
+
+
+    # FOOTER START
+    footer(pdf)
+    # FOOTER END
+    # THIRD PAGE END
+
+########################################################################################################################
+
+    # FOURTH PAGE START
+    pdf.showPage()
+    # HEADER START
+    header(pdf)
+    # HEADER END
+
+    # TITLE_4 START
+    title(pdf, text="CONTROLLO DI GESTIONE", y_offset=680)
+    # TITLE_4 END
+
+    # DESCRIPTIONS + IMG START
+
+    descriptions(pdf, text_1="Analisi Assenze Liberi Professionisti", text2="", x_offset=50, y_offset=660)
+    # Draw Altair chart image under the description
+    chart_path = data_to_chart.analisi_assenze_liberi_professionisti()
+    #chart_path = r'pngs_of_charts/chart_7.png'
+    draw_chart_under_description(pdf, chart_path, y_offset=670, logo_width=450, logo_height=200)
+
+    descriptions(pdf, text_1="Timesheet Mese", text2="", x_offset=50, y_offset=450)
+    chart_path = data_to_chart.timesheet_mese()
+    #chart_path = r'pngs_of_charts\chart_8.png'
+    draw_chart_under_description(pdf, chart_path, y_offset=465, logo_width=450, logo_height=180)
+
+    descriptions(pdf, text_1="BU/h", text2="", x_offset=50, y_offset=250)
+    chart_path = data_to_chart.bu_h_pie()
+    #chart_path = r'pngs_of_charts\chart_9.png'
+    draw_chart_under_description(pdf, chart_path, y_offset=265, logo_width=450, logo_height=130)
+
+    # DESCRIPTIONS + IMG END
+
+    # FOOTER START
+    footer(pdf)
+    # FOOTER END
+    # FOURTH PAGE END
+########################################################################################################################
+    # FIFTH PAGE START
+    pdf.showPage()
+    # HEADER START
+    header(pdf)
+    # HEADER END
+
+    # BOX_1 START
+
+    num_boxes = 2
+    box_titles = ["OBIETTIVI 2023", "COMMENTI"]
+    box_values = ["TESTO", "TESTO"]
+    box_width = 220
+    box_height = 80
+
+    draw_boxes(pdf, num_boxes, box_titles, box_values, box_width, box_height, current_y_offset=600)
+    # BOX_1 END
 
 
     # FOOTER START
