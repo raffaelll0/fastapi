1af852ac3ce1e47e7fbc8297a98ef072ef6e0a3a
raffaele.loglisci <dev03@ormalab.it>
2023-11-22 10:28:54 +0100

    some unnnecessary functions have been deleted, only 3 are needed, creation of pdf(in bg), waiting for it to be ready and uploading it on monday.com

--- myapi.py
+++ myapi.py
@@ -1,28 +1,3 @@
-# from fastapi import FastAPI, HTTPException
-# from fastapi.responses import StreamingResponse
-# from io import BytesIO
-# from monday_data_extraction import pdf_gen
-#
-#
-# from fastapi import FastAPI, HTTPException
-# from fastapi.responses import FileResponse
-# from fastapi.staticfiles import StaticFiles
-# from io import BytesIO
-# from monday_data_extraction import pdf_gen
-
-#app = FastAPI()
-
-
-# @app.get("/")
-# def download_blank_pdf():
-#     #call the genera pdf function in the main file
-#     pdf_bytes = pdf_gen.create_pdf(file_path="pdf_with_image.pdf")
-#     # Return the PDF as a response
-#     return StreamingResponse(BytesIO(pdf_bytes), media_type="application/pdf", headers={"Content-Disposition": "attachment;filename=blank.pdf"})
-#
-# download_blank_pdf()
-
-
 import os
 import time
 from fastapi import FastAPI, BackgroundTasks, HTTPException, Request
@@ -82,55 +57,9 @@ def generate_pdf(background_tasks: BackgroundTasks):
         else:
             return {"message": "PDF is not ready yet. Try again later."}
 
-@app.get("/upload_to_monday")
-# def upload_pdf_to_monday():
-#     """
-#     THIS FUNCTION MAKES A QUERY TO THE ITEM ID WE PREVIOUSLY HAVE CLICKED(BTN)
-#     DEFINES IN WICH COLUMN TO UPLOAD THE PDF, (IN THIS CASE 'file')
-#     AND THEN USES THE PATH OF THE PDF IN THE pdf_view, THIS FUNCTION WORKS IF IT
-#     IS CALLED IN THE pdf_view SINCE WE HAVE THE PATH INSIDE OF IT
-#
-#
-#     Args:
-#         apiKey: key to access monday.com via api
-#         apiUrl: url of monday.com
-#         headers: returns a keyvalue dictionary with the api key
-#         query: this defines the steps to add the file with in the specific column
-#         payload: via query it defines where to upload the file
-#
-#
-#
-#     Returns:
-#          response
-#
-#     """
-#     print("upload pdf....")
-#
-#     pdf_path = "pdf_with_image.pdf"
-#     query = 'mutation add_file ($file: File!) {add_file_to_column(file: $file, item_id: 4494285664, column_id: "file") {id}}'
-#     print("query passed....")
-#     data = {'query': query}
-#     files = [('variables[file]', ('hello.pdf', open(pdf_path, 'rb'), 'contenttype'))]
-#     response = requests.request("POST", apiUrl, headers=headers, data=data, files=files)
-#     print(response.text)
 
 
 
-@app.post("/webhook")
-async def webhook(request: Request):
-    try:
-        data = await request.json()
-        challenge = data.get('challenge')
-
-        if challenge:
-            return JSONResponse(content={'challenge': challenge})
-
-        pdf_path = generate_pdf()
-        # Add your logic for handling the POST request here
-        upload_pdf_to_monday(pdf_path)
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
 
 
